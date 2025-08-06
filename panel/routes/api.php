7fc3aa413a54c11a07feaf41cd5d18c93b4e99a5
NeyRoxDev <136857542+NeyRoxDev@users.noreply.github.com>
2025-08-06 14:40:43 +0200

    feat: manage user preferences

--- panel/routes/api.php
+++ panel/routes/api.php
@@ -1,1 +1,10 @@
-<?php // Routes API ici ?>
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserPreferencesController;
+
+Route::middleware('auth')->group(function () {
+    Route::get('/preferences', [UserPreferencesController::class, 'index']);
+    Route::post('/preferences', [UserPreferencesController::class, 'store']);
+});
+
